28a269e1dcdf1ed2f95970651c4787df8107b185
Gabriel <gjimenez.com@gmail.com>
2015-06-29 15:34:06 -0600

    Cambios de texto.

--- ProyectoFinal_PHP_II_2015_/index.php
+++ ProyectoFinal_PHP_II_2015_/index.php
@@ -69,7 +69,7 @@
 
         <!-- Jumbotron Header -->
         <header class="jumbotron hero-spacer">
-            <h1>A Warm Welcome!</h1>
+            <h1>Bienvenido al Restaurante!</h1>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.</p>
             <p><a class="btn btn-primary btn-large">Call to action!</a>
             </p>
@@ -149,7 +149,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
+                    <p>Derechos Reservados &copy; Restaurante <?php date('Y')?></p>
                 </div>
             </div>
         </footer>
